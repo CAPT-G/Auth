2fe2a9478a5cd617b48eecee3020483b535d48a1
CAPTAIN G <142211497+CAPT-G@users.noreply.github.com>
2025-06-02 04:09:07 -0400

    Update settings.py

--- AuthAccountProj/settings.py
+++ AuthAccountProj/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['accountauth-main.onrender.com','account-auth-rouge.vercel.app']
+ALLOWED_HOSTS=['https://auth-o84k.onrender.com']
 
 # Application definition
 
