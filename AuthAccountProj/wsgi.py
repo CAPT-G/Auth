b7c6d0cc2deed8e8e2d8a1431a574b0db65e3ea9
CAPTAIN G <142211497+CAPT-G@users.noreply.github.com>
2025-06-02 04:02:37 -0400

    Update wsgi.py

--- AuthAccountProj/wsgi.py
+++ AuthAccountProj/wsgi.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(__file__))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authAccountProj
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authAccountProj")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
