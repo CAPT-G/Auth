7283b02b09db147740a0994e360253dd67126986
CAPTAIN G <142211497+CAPT-G@users.noreply.github.com>
2025-06-02 03:54:50 -0400

    Update wsgi.py

--- AuthAccountProj/wsgi.py
+++ AuthAccountProj/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for AccountAuth project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+# project-level wsgi.py (next to manage.py)
+import sys
 import os
 
+sys.path.append(os.path.dirname(__file__))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authSystem.settings")
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AccountAuth.settings')
-
 application = get_wsgi_application()
